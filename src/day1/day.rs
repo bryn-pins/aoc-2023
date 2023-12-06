3d765636a10795da8a2fea37e8b004a246d30fac
Bryn Thomas <bryn.thomas@planninginspectorate.gov.uk>
2023-12-06 18:01:27 +0000

    tidy up day1

--- src/day1/day.rs
+++ src/day1/day.rs
@@ -1,149 +1,78 @@
-use crate::helper::read_file;
+use crate::helper;
 use std::collections::HashMap;
 
-const NUMBERS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const NUMBER_STRINGS: [&str; 20] = [
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "zero", "one", "two", "three", "four",
     "five", "six", "seven", "eight", "nine",
 ];
 
 pub fn part1() {
-    println!("called `day1::part1()`");
-
-    let input;
-
-    match read_file("src/day1/input.txt") {
-        Ok(contents) => {
-            input = contents;
-        }
-        Err(err) => {
-            eprintln!("error with file: {}", err);
-            return;
-        }
-    }
+    let input = helper::read_file("src/day1/input.txt").unwrap();
 
     let mut result = Vec::new();
-
+    
     for line in input.lines() {
         let mut line_chars = Vec::new();
 
-        for c in line.chars().filter(|&c| NUMBERS.contains(&c)) {
+        // push occurrence of allowed chars 
+        for c in line.chars().filter(|&c| NUMBER_STRINGS.contains(&c.to_string().as_str())) {
             line_chars.push(c);
         }
 
-        result.push(chars_to_string(&first_and_last(line_chars)));
+        // get first and last chars and combine into string
+        result.push(helper::chars_to_string(&first_and_last(line_chars)));
     }
 
-    let mut ints = Vec::new();
-    for string in result {
-        ints.push(string_to_int(&string));
-    }
-    let sum: i32 = ints.iter().sum();
-
-    println!("sum {}", sum);
-}
-
-pub fn part2() {
-    println!("called `day1::part2()`");
-
-    let input: String = read_file("src/day1/input.txt").unwrap();
-
-    let mut result = Vec::new();
-
-    for line in input.lines() {
-        let mut map: HashMap<&str, Vec<usize>> = HashMap::new();
-
-        for &search in NUMBER_STRINGS.iter() {
-            let mut occurances = Vec::new();
-
-            for (start, _) in line.match_indices(search) {
-                let found = start;
-                occurances.push(found);
-            }
-
-            if (occurances.len() > 0) {
-                map.insert(search, occurances);
-            }
-        }
-
-        let (mut min_key, mut min_val) = ("", usize::MAX);
-        let (mut max_key, mut max_val) = ("", usize::MIN);
-
-        for (key, value) in map.iter() {
-            if let Some(min) = value.iter().min() {
-                if *min <= min_val {
-                    min_key = key;
-                    min_val = *min;
-                }
-            }
-
-            if let Some(max) = value.iter().max() {
-                if *max >= max_val {
-                    max_key = key;
-                    max_val = *max;
-                }
-            }
-        }
-
-        let min = string_to_int(min_key);
-        let max = string_to_int(max_key);
-        let combined: String = min.to_string() + &max.to_string();
-        result.push(string_to_int(&combined));
-    }
-
-    let sum: i32 = result.iter().sum();
-
-    println!("sum {}", sum);
+    // convert strings to ints and sum
+    let sum: usize = result.iter().map(|s| helper::string_to_int(&s)).sum();
+    println!("day1:1 sum {}", sum);
 }
 
 fn first_and_last(input: Vec<char>) -> Vec<char> {
     match input.len() {
-        0 => Vec::new(),
-        1 => {
-            vec![input[0], input[0]]
-        }
-        _ => {
-            vec![input[0], input[input.len() - 1]]
-        }
+        0 => vec![],
+        1 => vec![input[0], input[0]],
+        _ => vec![input[0], input[input.len() - 1]],
     }
 }
 
-fn chars_to_string(input: &[char]) -> String {
-    input.iter().collect()
+pub fn part2() {
+    let input: String = helper::read_file("src/day1/input.txt").unwrap();
+
+    let mut result = Vec::new();
+
+    for line in input.lines() {
+        let occurrence_map = helper::get_occurrences_of_search(line, &NUMBER_STRINGS);
+        let (min_key, max_key) = find_min_max(occurrence_map);
+        result.push(helper::combine_strings_as_ints(&min_key, &max_key));
+    }
+
+    let sum: usize = result.iter().sum();
+
+    println!("day1:2 sum {}", sum);
 }
 
-fn string_to_int(string: &str) -> i32 {
-    let spelling_to_num: HashMap<&str, i32> = [
-        ("zero", 0),
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
-    ]
-    .iter()
-    .cloned()
-    .collect();
+fn find_min_max(map: HashMap<String, Vec<usize>>) -> (String, String) {
+    let (mut min_key, mut min_val) = (String::new(), usize::MAX);
+    let (mut max_key, mut max_val) = (String::new(), usize::MIN);
 
-    match string.parse::<i32>() {
-        Ok(num) => num,
-        Err(_err) => {
-            let mut result = 0;
-
-            match spelling_to_num.get(string) {
-                Some(res) => {
-                    result = *res;
-                }
-                None => {
-                    panic!("shouldn't get here...")
-                }
+    for (key, value) in map.iter() {
+        // find the min index this term occurred, use that if lowest so far
+        if let Some(min) = value.iter().min() {
+            if *min <= min_val {
+                min_key = key.clone();
+                min_val = *min;
             }
+        }
 
-            result
+        // find the max index this term occurred, use that if highest so far
+        if let Some(max) = value.iter().max() {
+            if *max >= max_val {
+                max_key = key.clone();
+                max_val = *max;
+            }
         }
     }
+
+    (min_key, max_key)
 }
--- src/helper.rs
+++ src/helper.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::io;
@@ -11,4 +12,59 @@ pub fn read_file(path: &str) -> Result<String, io::Error> {
     // println!("reading file: {}", full_path.to_string_lossy());
 
     fs::read_to_string(full_path)
+}
+
+pub fn string_to_int(string: &str) -> usize {
+    let mappings: HashMap<&str, usize> = [
+        ("zero", 0),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    match string.parse::<usize>() {
+        Ok(num) => num,
+        Err(_err) => *mappings
+            .get(string)
+            .unwrap_or_else(|| panic!("shouldn't get here... {}", string)),
+    }
+}
+
+pub fn chars_to_string(input: &[char]) -> String {
+    input.iter().collect()
+}
+
+pub fn combine_strings_as_ints(val1: &str, val2: &str) -> usize {
+    let combined = format!("{}{}", string_to_int(val1), string_to_int(val2));
+    string_to_int(&combined)
+}
+
+pub fn get_occurrences_of_search(
+    input: &str,
+    search_terms: &[&str],
+) -> HashMap<String, Vec<usize>> {
+    let mut map: HashMap<String, Vec<usize>> = HashMap::new();
+
+    // get start index for occurrences of match from search_terms
+    for &search in search_terms.iter() {
+        let occurrences: Vec<usize> = input
+            .match_indices(search)
+            .map(|(start, _)| start)
+            .collect();
+
+        if !occurrences.is_empty() {
+            map.insert(search.to_string(), occurrences);
+        }
+    }
+
+    map
 }
